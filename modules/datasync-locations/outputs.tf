084b27508e83aa21807b3832908d6548c5168acb
Kawsar <kawsark@amazon.com>
2024-03-19 08:37:06 -0400

    added role arn output

--- modules/datasync-locations/outputs.tf
+++ modules/datasync-locations/outputs.tf
@@ -5,3 +5,8 @@ output "s3_locations" {
 output "efs_locations" {
   value = aws_datasync_location_efs.efs_location
 }
+
+output "datasync_role_arn" {
+  value = { for k, role in aws_iam_role.datasync_role_s3 : k => role.arn }
+}
+
