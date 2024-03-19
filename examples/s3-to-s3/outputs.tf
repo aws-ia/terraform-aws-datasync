3478ecc0a244a0cf1f5b76693eb4fb755d6e2bb5
Kawsar <kawsark@amazon.com>
2024-03-19 08:37:06 -0400

    added role arn output

--- examples/s3-to-s3/outputs.tf
+++ examples/s3-to-s3/outputs.tf
@@ -5,3 +5,7 @@ output "my_s3_locations" {
 output "backup_tasks" {
   value = module.backup_tasks.datasync_tasks
 }
+
+output "datasync_role_arn" {
+  value = module.s3_location.datasync_role_arn["anycompany-bu1-appl1-logs"]
+}
