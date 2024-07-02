aacdc77acfb8bdd04ad33bd5247c922a417c4e8b
harsheetas <surampa@amazon.com>
2024-07-02 16:11:51 -0400

    Added description to the output

--- examples/s3-to-s3-cross-account/outputs.tf
+++ examples/s3-to-s3-cross-account/outputs.tf
@@ -1,24 +1,25 @@
 output "datasync_task_arn" {
   value       = module.backup_tasks.datasync_tasks
   description = "Datasync Task ARN"
-  sensitive   = false
 }
 
 output "my_s3_source_locations" {
-  value       = module.s3_source_location.s3_locations.source-bucket
-  description = "DataSync S3 Source Location ARNs"
+  description = "DataSync S3 Source Location ARN"
+  value       = module.s3_source_location.s3_locations.source-bucket  
 }
 
 output "my_s3_dest_locations" {
+  description = "DataSync S3 Destination Location ARN"
   value       = module.s3_dest_location.s3_locations.dest-bucket
-  description = "DataSync S3 Destination Location ARNs"
 }
 
 output "datasync_src_role_arn" {
+  description = "DataSync Source S3 Access IAM role ARN"
   value = aws_iam_role.datasync_source_s3_access_role.arn
 }
 
 output "datasync_dest_role_arn" {
+  description = "DataSync Destination S3 Access IAM role ARN"
   value = module.s3_dest_location.datasync_role_arn["dest-bucket"]
 }
 
