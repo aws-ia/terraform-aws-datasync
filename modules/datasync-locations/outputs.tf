4e91676fdb5c9121583bed1442d84a27ee33cb27
harsheetas <surampa@amazon.com>
2024-07-02 15:01:57 -0400

    Added description to the output values

--- modules/datasync-locations/outputs.tf
+++ modules/datasync-locations/outputs.tf
@@ -1,15 +1,15 @@
 output "s3_locations" {
-  description = "DataSync S3 Location ARN"
   value = aws_datasync_location_s3.s3_location
+  description = "DataSync S3 Location ARN"
 }
 
 output "efs_locations" {
-  description = "DataSync EFS Location ARN"
   value = aws_datasync_location_efs.efs_location
+  description = "DataSync EFS Location ARN"
 }
 
 output "datasync_role_arn" {
-  description = "DataSync Task ARN"
   value = { for k, role in aws_iam_role.datasync_role_s3 : k => role.arn }
+  description = "DataSync Task ARN"
 }
 
