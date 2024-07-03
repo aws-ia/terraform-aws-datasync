aec831c9956d7cdfedf1c58de343336d1e97283a
harsheetas <surampa@amazon.com>
2024-07-03 17:41:19 -0400

    Fixed CKV2_AWS_67 reported issues regarding CMK KMS

--- examples/efs-to-s3/vpc.tf
+++ examples/efs-to-s3/vpc.tf
@@ -6,10 +6,10 @@ data "aws_availability_zones" "available" {}
 #VPC flow logs enabled. Skipping tfsec bug https://github.com/aquasecurity/tfsec/issues/1941
 #tfsec:ignore:aws-ec2-require-vpc-flow-logs-for-all-vpcs
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = ">= 1.0.0"
+  source  = "git::https://github.com/terraform-aws-modules/terraform-aws-vpc.git?ref=26c38a66f12e7c6c93b6a2ba127ad68981a48671"  # commit hash of version 5.0.0 
 
   cidr = var.vpc_cidr_block
+
 
   azs             = slice(data.aws_availability_zones.available.names, 0, (var.subnet-count))
   private_subnets = [for subnet in range(var.subnet-count) : cidrsubnet(var.vpc_cidr_block, 8, subnet)] # For Private subnets
